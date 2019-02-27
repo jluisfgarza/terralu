6c207e8cf9ec3b0f8cf6aa8671f69e02920fd698
abrilgzz <abril.gonzalezr@gmail.com>
2019-02-27 02:24:41 -0600

    setup gh pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Terralú under construction
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
