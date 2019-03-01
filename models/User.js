3bc82d306d0ce55c6a24073c5abf043ef68832a1
Andrea Tamez <andreatamez@Andreas-MacBook-Pro.local>
2019-02-28 21:11:21 -0600

    Crear UserSchema y User Session

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    default: ''
+  },
+  lastName: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  password: {
+    type: String,
+    default: ''
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+});
+
+UserSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
