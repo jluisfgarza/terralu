a21d3c688e541cc880eac1557536208622fafca1
jluisfgarza <jluisfgarza@gmail.com>
2019-03-01 01:22:46 -0600

    restore project to working state after user git error

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const {buildSchema} = require('graphql');
 const {router : productsRouter} = require('./routes/productsRouter');
 const {PORT, DATABASE_URL} = require('./config');
 const passport = require('passport');
@@ -19,63 +17,7 @@ app.use(express.static('public'));
 app.use('/api/', productsRouter);
 app.use(bodyParser.json());
 
-// app.get('/', (req, res, next) => {
-//   res.send('Hello World');
-// })
-
-app.use('/graphiql', graphqlHttp({
-  schema: buildSchema(`
-    type Product {
-      _id: ID!
-      title: String!
-      description: String!
-      price: Float!
-      inStock: Int!
-      numbBought: Int!
-    }
-
-    type RootQuery {
-      products: [Product!]!
-    }
-
-    input ProductInput {
-      title: String!,
-      description: String!,
-      price: Float!,
-      inStock: Int!,
-      numBought: Int!
-    }
-
-    type RootMutation {
-      createProduct(productInput: ProductInput): Product
-    }
-
-    schema {
-      query: RootQuery
-      mutation: RootMutation
-    }
-  `),
-  rootValue: {
-    products: () => {
-      return products;
-    },
-    createProduct: (args) => {
-      const product = {
-        _id: Math.random().toString(),
-        title: args.productInput.title,
-        description: args.productInput.description,
-        price: +args.productInput.price,
-        inStock: args.productInput.inStock,
-        numbBought: args.productInput.numbBought
-      }
-      products.push(product);
-      return product;
-    }
-  },
-  graphiql: true
-}));
-
-
+/*
 passport.use(new LocalStrategy(
   function(username, password, done) {
     Users.findOne({ username: username }, function(err, user) {
@@ -90,24 +32,21 @@ passport.use(new LocalStrategy(
     });
   }
 ));
-
 passport.serializeUser(function(user, cb) {
   cb(null, user.id);
 });
-
 passport.deserializeUser(function(id, cb) {
   db.users.findById(id, function (err, user) {
     if (err) { return cb(err); }
     cb(null, user);
   });
 });
-
 app.post('/login',
   passport.authenticate('local', { successRedirect: '/',
                                    failureRedirect: '/login',
                                    failureFlash: true })
 );
-
+*/
 
 // this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
