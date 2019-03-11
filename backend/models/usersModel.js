3e959ce7bba33253a06a0b15e54c49a008736871
abrilgzz <abril.gonzalezr@gmail.com>
2019-03-10 19:28:30 -0600

    updated auth api with passport jwt

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -1,14 +1,23 @@
-const mongoose =  require('mongoose');
-mongoose.Promise = global.Promise;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const dbUsers = mongoose.Schema({
-    username : {type : String, required : true},
-    email : {type : String, required : true},
-    password : {type : String, required : true},
+// Create Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
-
-const Users = mongoose.model("Users", dbUsers, "terralu");
-
-module.exports = {
-    Users : Users
-};
+module.exports = User = mongoose.model("users", UserSchema);
---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require("passport");
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Body Parser Middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").MONGO_URI;
+const localdb = require("./config/keys").LOCALDATABASE;
+const port = require("./config/keys").PORT;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db, {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`))
