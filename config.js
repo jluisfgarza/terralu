77bd291c77005acebc91b95ba15fb530aa67da66
Andrea Tamez <andreatamez@Andreas-MacBook-Pro.local>
2019-03-19 16:23:01 -0700

    hello

--- config.js
+++
@@ -1,2 +0,0 @@
-exports.DATABASE_URL = 'mongodb://localhost/products';
-exports.PORT = process.env.PORT || '8080';
--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGO_URI: `mongodb://admin:Abj8pvcyp8vUT2w9pDmf@terralu-store-shard-00-00-ocsyy.mongodb.net:27017,terralu-store-shard-00-01-ocsyy.mongodb.net:27017,terralu-store-shard-00-02-ocsyy.mongodb.net:27017/test?ssl=true&replicaSet=terralu-store-shard-0&authSource=admin&retryWrites=true`,
-  LOCALDATABASE: 'mongodb://localhost/products',
-  PORT: process.env.PORT || 5000,
-  secretOrKey: "secret"
-};
