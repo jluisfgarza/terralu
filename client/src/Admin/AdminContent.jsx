01d32bba647be0b570768294882345da0e23c6f7
abrilgzz <abril.gonzalezr@gmail.com>
2019-03-21 00:24:34 -0600

    admin views and add prodit init

--- client/src/Admin/AdminContent.jsx
+++ client/src/Admin/AdminContent.jsx
@@ -4,19 +4,20 @@ import Typography from "@material-ui/core/Typography";
 // Styles
 import { withStyles } from "@material-ui/core/styles";
 import style from "./adminStyle";
+import DemoTable from "./DemoTable";
 
 class AdminContent extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Typography variant="h4" gutterBottom>
-            {this.props.url}
-          </Typography>
-        </main>
-      </div>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography variant="h4" gutterBottom>
+          {this.props.url}
+        </Typography>
+        <br />
+        <DemoTable url={this.props.url} />
+      </main>
     );
   }
 }
