5c1fb7b26ba59e7f8324111c15362a9e472d7c3a
jluisfgarza <jluisfgarza@gmail.com>
2019-05-04 21:03:49 -0500

    removed build from gitignore

---
+++ client/build/precache-manifest.662e623640480649d2f9c099fa8babab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67c0f7edf54072194f3afe18bfe2062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbbedfb1af3b45ef0776",
+    "url": "/static/css/2.4aa08784.chunk.css"
+  },
+  {
+    "revision": "5d36fe8842f3b4fcb72d",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "cbbedfb1af3b45ef0776",
+    "url": "/static/js/2.d25ac5c9.chunk.js"
+  },
+  {
+    "revision": "5d36fe8842f3b4fcb72d",
+    "url": "/static/js/main.5fdabbdf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79cabd8e744cd493c1297505d61b755b",
+    "url": "/static/media/hero2.79cabd8e.jpg"
+  },
+  {
+    "revision": "fac30b22183604000711870aec9c5827",
+    "url": "/static/media/hero3.fac30b22.jpg"
+  },
+  {
+    "revision": "e9886a3f14f7a0279e9f84076be70a72",
+    "url": "/static/media/logo.e9886a3f.jpg"
+  }
+]);
